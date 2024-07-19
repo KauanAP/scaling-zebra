e4b651dfe23ca4f53c1f1a83ab0404a46129212b
KauanAP <135459913+KauanAP@users.noreply.github.com>
2024-07-19 19:54:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/scaling-zebra",
   test: {
     globals: true,
     environment: 'jsdom',
